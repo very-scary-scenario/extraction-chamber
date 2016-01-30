80d64ab6cad29c79158504e0ee85126a19cbb48a
colons <code@colons.co>
2016-01-30 15:05:58 +0000

    Implement the actuator mechanic (but it's broken af).

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 var game;
 var liquidElement = document.getElementById('liquid');
 var bottleList = document.getElementById('bottles');
+var actuatorList = document.getElementById('actuators');
 
 function extend(base, sub) {
   sub.prototype = Object.create(base.prototype);
@@ -11,12 +12,27 @@ function extend(base, sub) {
   });
 }
 
+function makeListEqual(list, count) {
+  while (count > list.querySelectorAll('li').length) {
+    list.appendChild(document.createElement('li'));
+  }
+  while (count > list.querySelectorAll('li').length) {
+    list.removeChild(querySelectorAll('li')[0]);
+  }
+}
+
 function Game() {
   var self = this;
   self.currentBottleMl = 0;
   self.bottleCount = 0;
 
   self.handleClick = function() {
+    new Actuator();
+    self.currentBottleMl += 1;
+    self.step();
+  };
+
+  self.handleActuator = function() {
     self.currentBottleMl += 1;
     self.step();
   };
@@ -24,21 +40,34 @@ function Game() {
   self.step = function() {
     if (self.currentBottleMl >= 30) {
       self.bottleCount += 1;
-      self.currentBottleMl = 0;
+      self.currentBottleMl -= 30;
     }
     self.update();
   };
 
   self.update = function() {
     liquidElement.style.height = ((self.currentBottleMl / 30) * 100).toString(10) + '%';
-
-    while (self.bottleCount > bottleList.querySelectorAll('li').length) {
-      bottleList.appendChild(document.createElement('li'));
-    }
-    while (self.bottleCount > bottleList.querySelectorAll('li').length) {
-      bottleList.removeChild(querySelectorAll('li')[0]);
-    }
+    makeListEqual(bottleList, self.bottleCount);
   };
+}
+
+function Actuator() {
+  var self = this;
+  self.element = document.createElement('li');
+  actuatorList.appendChild(self.element);
+
+  self.engage = function() {
+    self.element.classList.add('engaged');
+    game.handleActuator();
+    setTimeout(self.disengage, 100);
+  };
+
+  self.disengage = function() {
+    self.element.classList.remove('engaged');
+    setTimeout(self.engage, 1000);
+  };
+
+  self.engage();
 }
 
 game = new Game();
