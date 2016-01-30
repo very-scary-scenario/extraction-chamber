49a7a976dd5e2ed576e34ab2e426ead712759419
colons <code@colons.co>
2016-01-30 19:29:56 +0000

    Minimum viable shop.

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
 var game;
 var liquidElement = document.getElementById('liquid');
 var bottleList = document.getElementById('bottles');
-
+var shopList = document.getElementById('shop');
+var shopElements = shopList.querySelectorAll('li');
 
 function extend(base, sub) {
   sub.prototype = Object.create(base.prototype);
@@ -16,8 +17,8 @@ function makeListEqual(list, count) {
   while (count > list.querySelectorAll('li').length) {
     list.appendChild(document.createElement('li'));
   }
-  while (count > list.querySelectorAll('li').length) {
-    list.removeChild(querySelectorAll('li')[0]);
+  while (count < list.querySelectorAll('li').length) {
+    list.removeChild(list.querySelectorAll('li')[0]);
   }
 }
 
@@ -26,10 +27,40 @@ function Game() {
   self.currentBottleMl = 0;
   self.bottleCount = 0;
   self.actuatorDelay = 1000;
-  self.actuators = [];
+  self.itemsOwned = {
+    actuator: 0,
+    battery: 0,
+    tank: 0,
+    doubler: 0,
+    quonk: 0,
+    wick: 0
+  };
+
+  self.itemConstructors = {
+    actuator: Actuator
+  };
+
+  self.purchase = function(itemName) {
+    new self.itemConstructors[itemName]();
+    self.itemsOwned[itemName] += 1;
+  };
+
+  self.purchaser = function(itemName) {
+    return function() {
+      var shopElement = shopList.querySelector('[data-item="' + itemName + '"]');
+      var value = parseInt(shopElement.getAttribute('data-value'), 10);
+      if (self.bottleCount >= value) {
+        self.purchase(itemName);
+        self.bottleCount -= value;
+        shopElement.setAttribute('data-value', (value * 2).toString(10));
+      } else {
+        alert('u hav no munny :<');
+      }
+      self.update();
+    };
+  };
 
   self.handleClick = function() {
-    new Actuator();
     self.currentBottleMl += 1;
     self.step();
   };
@@ -47,16 +78,28 @@ function Game() {
     self.update();
   };
 
+  self.updateShop = function() {
+    for (var i = 0; i < shopElements.length; i++) {
+      var shopElement = shopElements[i];
+      var itemName = shopElement.getAttribute('data-item');
+      var owned = self.itemsOwned[itemName];
+      if (owned) {
+        shopElement.setAttribute('data-owned', owned.toString(10));
+      }
+    }
+  };
+
   self.update = function() {
     liquidElement.style.height = ((self.currentBottleMl / 30) * 100).toString(10) + '%';
     makeListEqual(bottleList, self.bottleCount);
+    self.updateShop();
   };
 }
 
 function Actuator() {
   var self = this;
   self.element = document.createElement('li');
-  document.getElementById('actuators-' + (game.actuators.length % 2).toString(10)).appendChild(self.element);
+  document.getElementById('actuators-' + (game.itemsOwned.actuator % 2).toString(10)).appendChild(self.element);
 
   self.engage = function() {
     self.element.classList.add('engaged');
@@ -69,12 +112,17 @@ function Actuator() {
     setTimeout(self.engage, game.actuatorDelay + Math.random() * (game.actuatorDelay/2));
   };
 
-  game.actuators.push(self);
   self.engage();
 }
 
 game = new Game();
 game.step();
+
+for (var i = 0; i < shopElements.length; i++) {
+  var shopElement = shopElements[i];
+  var itemName = shopElement.getAttribute('data-item');
+  shopElement.addEventListener('click', game.purchaser(itemName));
+}
 
 document.addEventListener('click', function(e) {
   e.preventDefault();
